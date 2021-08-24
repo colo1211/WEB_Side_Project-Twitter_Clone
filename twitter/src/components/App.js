594c2fe289f4d0c7057150909fbfb5f26d59a99a
colo1211 <colo1211@naver.com>
2021-08-24 11:22:01 +0900

    Router.js 에서 만든 state를 App.js 로 옮겨서 props로 Router로 전달

--- twitter/src/components/App.js
+++ twitter/src/components/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import AppRouter from './Router.js';
+import React, { useState } from 'react';
+import AppRouter from 'components/Router.js';
+import {authService} from 'fbase.js';
+
 function App() {
+  
+  console.log(authService); 
+  const [isLogin, setIsLogin] = useState(false); // 로그인이 되어 있는지 체크하는 state 
+  
   return (
-      <AppRouter/>
+    <>
+      <AppRouter isLogin={isLogin}/>
+      <footer>&copy; {new Date().getFullYear()} Twitter </footer>
+    </>
   );
 }
 
