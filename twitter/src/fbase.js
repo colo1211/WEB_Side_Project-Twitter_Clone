dd1cb5300588690ea3622320a8c601e56febf557
colo1211 <colo1211@naver.com>
2021-08-25 22:41:21 +0900

    Db 사용을 위한 firebase/firestore 를 import 후 dbService로 export

--- twitter/src/fbase.js
+++ twitter/src/fbase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import 'firebase/auth'; 
+import 'firebase/firestore'; 
 import dotenv from 'dotenv';
 dotenv.config(); 
 // console.log(process.env.REACT_APP_API_KEY); 
@@ -18,7 +19,7 @@ const firebaseConfig = {
   
   export const firebaseInstance = firebase; 
   export const authService = firebase.auth();
-   
+  export const dbService = firebase.firestore(); 
 
 
 
