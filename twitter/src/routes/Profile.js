2607a45dc07b70605a365cfba9edf8e750ca4494
colo1211 <colo1211@naver.com>
2021-08-25 15:11:50 +0900

    LogOut : signOut(), history.push('/');

--- twitter/src/routes/Profile.js
+++ twitter/src/routes/Profile.js
@@ -1,4 +1,18 @@
+import { authService } from 'fbase';
 import React from 'react'; 
+import { useHistory } from 'react-router-dom';
 
-const Profile = ()=> <span>Profile</span>; 
-export default Profile; 
+export default () => {
+
+    let history = useHistory();
+    const onLogOutClock =()=>{
+        authService.signOut(); // 로그아웃을 하는 Firebase 함수 .signOut();
+        // history.push('/'); 
+    }
+    return (
+        <>
+            <span>My Profile</span>
+            <button onClick = {onLogOutClock}>Log Out</button> 
+        </>
+    )
+}
