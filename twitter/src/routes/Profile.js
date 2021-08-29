657de99346c577d43519913b3847ba776db6678c
colo1211 <colo1211@naver.com>
2021-08-29 20:07:07 +0900

    Profile 내가 쓴 글 필터링된 결과 기능 추가

--- twitter/src/routes/Profile.js
+++ twitter/src/routes/Profile.js
@@ -1,17 +1,32 @@
-import { authService } from 'fbase';
-import React from 'react'; 
+import { authService, dbService } from 'fbase';
+import React, { useEffect, useState } from 'react'; 
 import { useHistory } from 'react-router-dom';
 
-export default ({isLogin ,setIsLogin}) => {
+export default ({isLogin ,setIsLogin, userObj}) => {
 
+    let myNweet = ''; 
     let history = useHistory();
+
     const onLogOutClock =()=>{
         authService.signOut(); // 로그아웃을 하는 Firebase 함수 .signOut();
         // setIsLogin(false); 
         history.push('/'); 
     }
+
+    const getMynweet = async() =>{
+         myNweet = await dbService.collection('nwitter')
+        .where("creatorId","==",userObj.uid)
+        .orderBy('createdAt','asc')
+        .get();
+        myNweet.docs.map((doc)=>{console.log(doc.data())}); // 데이터를 가져오는 방법
+    }
+
+    useEffect(()=>{
+        getMynweet(); 
+    },[]);
+
     return (
-        <>
+        <>       
             <span>My Profile</span>
             <button onClick = {onLogOutClock}>Log Out</button> 
         </>
