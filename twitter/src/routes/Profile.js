001614bec350a6363e729a22b12c4d5950a53cec
colo1211 <colo1211@naver.com>
2021-08-30 13:54:54 +0900

    .

--- twitter/src/routes/Profile.js
+++ twitter/src/routes/Profile.js
@@ -2,33 +2,39 @@ import { authService, dbService } from 'fbase';
 import React, { useEffect, useState } from 'react'; 
 import { useHistory } from 'react-router-dom';
 
-export default ({isLogin ,setIsLogin, userObj}) => {
+export default ({isLogin ,setIsLogin}) => {
 
-    let myNweet = ''; 
+    // // console.log(userObj.uid); 
+
+    // let temp; 
+
+    // const getMynweet = async() =>{
+    //     temp = await dbService.collection('nwitter')
+    //     .where("creatorId", "==", userObj.uid)
+    //     .orderBy('createdAt','asc')
+    //     .get();
+    //     console.log(temp.docs); 
+    //     // temp.docs.map((doc)=> console.log(doc.data())); 
+    // }
+
+    // useEffect(()=>{
+    //     getMynweet(); 
+        
+    // },[]);
+    
     let history = useHistory();
-
     const onLogOutClock =()=>{
         authService.signOut(); // 로그아웃을 하는 Firebase 함수 .signOut();
         // setIsLogin(false); 
         history.push('/'); 
     }
 
-    const getMynweet = async() =>{
-         myNweet = await dbService.collection('nwitter')
-        .where("creatorId","==",userObj.uid)
-        .orderBy('createdAt','asc')
-        .get();
-        myNweet.docs.map((doc)=>{console.log(doc.data())}); // 데이터를 가져오는 방법
-    }
-
-    useEffect(()=>{
-        getMynweet(); 
-    },[]);
 
     return (
         <>       
             <span>My Profile</span>
             <button onClick = {onLogOutClock}>Log Out</button> 
+                  
         </>
     )
 }
