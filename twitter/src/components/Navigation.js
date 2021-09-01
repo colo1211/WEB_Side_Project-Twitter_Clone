ff2a6fb2d562c9b1f7ea894130ba94f4797770f2
colo1211 <colo1211@naver.com>
2021-09-01 14:53:22 +0900

    userName 을 변경하기 위한 userObj props 추가

--- twitter/src/components/Navigation.js
+++ twitter/src/components/Navigation.js
@@ -1,12 +1,14 @@
+import { authService } from 'fbase';
 import React from 'react'; 
 import {Link} from 'react-router-dom'; 
 
-const Navigation = () => {
+const Navigation = ({userObj}) => {
+    // console.log(userObj.displayName);  
     return(
     <div>
         <ul>
             <li><Link to='/'> Home </Link></li>
-            <li><Link to='/profile'> My Profile </Link></li>
+            <li><Link to='/profile'> { userObj.displayName } 의 Profile </Link></li>
         </ul>
     </div>
     )
