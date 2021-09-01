5a9f30f7fdde9a8e46d8b5e2e145c8cdcae0aa63
colo1211 <colo1211@naver.com>
2021-09-01 13:21:25 +0900

    Profile Edit

--- twitter/src/routes/Profile.js
+++ twitter/src/routes/Profile.js
@@ -2,9 +2,7 @@ import { authService, dbService } from 'fbase';
 import React, { useEffect, useState } from 'react'; 
 import { useHistory } from 'react-router-dom';
 
-export default ({isLogin ,setIsLogin}) => {
-
-    // // console.log(userObj.uid); 
+export default ({isLogin ,setIsLogin, userObj}) => {
 
     // let temp; 
 
@@ -21,18 +19,36 @@ export default ({isLogin ,setIsLogin}) => {
     //     getMynweet(); 
         
     // },[]);
-    
+
     let history = useHistory();
+    const [ newDisplayName, setNewDisplayName ] = useState(userObj.displayName); 
+    
     const onLogOutClock =()=>{
         authService.signOut(); // 로그아웃을 하는 Firebase 함수 .signOut();
         // setIsLogin(false); 
         history.push('/'); 
     }
 
+    const onChange = (e) => {
+        setNewDisplayName(e.target.value);
+    }
+
+    const onSubmit = async (e) =>{ // update displayName
+        e.preventDefault();
+        if (userObj.displayName !== newDisplayName) { // 기존의 displayName과 newDisplayName 과 다를때만
+            await userObj.updateProfile({ // 인자 1. displaName 2. Photo URL
+                displayName : newDisplayName
+            }); 
+            console.log(userObj.displayName);
+        }
+    }
 
     return (
         <>       
-            <span>My Profile</span>
+            <form> 
+                <input onChange={onChange} type='text' placeholder='Display Name' value={newDisplayName}/>
+                <input onClick={onSubmit}type='submit' value='Update Profile'/>
+            </form>
             <button onClick = {onLogOutClock}>Log Out</button> 
                   
         </>
